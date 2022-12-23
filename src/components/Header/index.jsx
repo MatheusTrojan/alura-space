17a5cb349734c8f5e6e73207321b56e158f17e47
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-23 15:51:22 -0300

    implementa o sass para estilizar a pagina

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import logo from "./logo.png"
 import search from "./search.png"
+import styles from "./Header.module.scss"
 
 export default function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <img src={logo} alt="Logo da Frida's Dog" />
-            <div>
-                <input type="text" placeholder="O que você procura?" />
+            <div className={styles.header__container}>
+                <input className={styles.header__input} type="text" placeholder="O que você procura?" />
                 <img src={search} alt="ícone de lupa" />
             </div>
         </header>
