7ae3374525d3e251d2ed02414eb7df324b3e7977
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-25 22:33:46 -0300

    adiciona o banner à pagina inicial

--- src/pages/InitialPage/index.jsx
+++ src/pages/InitialPage/index.jsx
@@ -1,11 +1,21 @@
+import banner from "./banner.png"
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
+import styles from "./InitialPage.module.scss"
 
 export default function InitialPage() {
     return (
         <>    
             <Header />
-            <Menu />
+            <main>
+                <section className={styles.main__section}>
+                    <Menu />    
+                    <div className={styles.main__section__image}>
+                        <h1>O melhor dog da cidade!</h1>
+                        <img src={banner} alt="Banner da marca frida's dog" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
