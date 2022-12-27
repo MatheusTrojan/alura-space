364b04bdeb84951fe6f136df704244be097f42cc
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-27 17:27:53 -0300

    altera fonte e reorganiza imagens

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
-import logo from "./logo.png"
+import logo from "./logo-sem-escrita.png"
 import search from "./search.png"
 import styles from "./Header.module.scss"
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <img src={logo} alt="Logo da Frida's Dog" />
+            <div className={styles.logo__container}>
+                <img src={logo} alt="Logo da Frida's Dog" />
+                <h2 className={styles.logo__container__title}>Frida's Dog</h2>
+            </div>
             <div className={styles.header__container}>
                 <input className={styles.header__input} type="text" placeholder="O que você procura?" />
                 <img src={search} alt="ícone de lupa" />
