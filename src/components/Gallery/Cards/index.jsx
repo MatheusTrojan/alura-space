76697ec9e69648d9e025c4ac02571281dae21809
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-31 16:03:35 -0300

    cria componente de card para a galeria

---
+++ src/components/Gallery/Cards/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+export default function Cards({ itens, styles }) {
+  return (
+    <ul className={styles.gallery__cards}>
+        {itens.map((item) => {
+        return (
+            <Card key={item.key} item={item} styles={styles}/>
+        )
+        })}    
+    </ul>
+  )
+}
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,35 +2,15 @@ import React from 'react'
 import Tags from '../Tags'
 import styles from "./Gallery.module.scss"
 import photos from "./photos.json"
-import open from "./open.png"
-import favorito from "./favorito.png"
+
+import Cards from './Cards'
 
 export default function Gallery() {
   return (
     <section className={styles.gallery}>
         <h2>Navegue pela Galeria</h2>
         <Tags />
-        <ul className={styles.gallery__cards}>
-          {photos.map((photo) => {
-            return (
-              <li key={photo.key} className={styles.gallery__card}>
-                <img 
-                  className={styles.gallery__image}
-                  src={photo.image}
-                  alt={photo.title}
-                />
-                <p className={styles.gallery__description}>{photo.title}</p>
-                <div>
-                  <p>{photo.credits}</p>
-                  <span>
-                    <img src={favorito} alt="ícone coração de curtir"/>
-                    <img src={open} alt="ícone de abrir modal"/>
-                  </span>
-                </div>
-              </li>
-            )
-          })}    
-        </ul>
+        <Cards itens={photos} styles={styles}/>
     </section>
   )
 }
