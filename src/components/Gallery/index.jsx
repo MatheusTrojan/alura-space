cf650e7da98e57a91f27d0aa24911761cae68868
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-03 17:58:38 -0300

    cria sistema de filtragem das fotos

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import Tags from '../Tags'
 import styles from "./Gallery.module.scss"
 import photos from "./photos.json"
-
 import Cards from './Cards'
+import { useState } from 'react'
 
 export default function Gallery() {
+
+  const [itens, setItens] = useState(photos)
+  const tags = [...new Set(photos.map((value) => value.tag))]
+
+  const filterPhotos = (tag) => {
+    const newPhotos = photos.filter((photo) => {
+      return photo.tag === tag;
+    })
+
+    setItens(newPhotos)
+  }
+
   return (
     <section className={styles.gallery}>
         <h2>Navegue pela Galeria</h2>
-        <Tags />
-        <Cards itens={photos} styles={styles}/>
+        <Tags tags={tags} filterPhotos={filterPhotos} setItens={setItens}/>
+        <Cards itens={itens} styles={styles}/>
     </section>
   )
 }
