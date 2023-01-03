e9a7d77ba8bfd507e178f78c81b40357cc23ecf6
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-03 17:40:05 -0300

    cria o arquivo jsconfig para facilitar importação de arquivos

--- src/pages/InitialPage/index.jsx
+++ src/pages/InitialPage/index.jsx
@@ -1,10 +1,10 @@
-import Header from "../../components/Header";
-import Menu from "../../components/Menu";
+import Header from "components/Header";
+import Menu from "components/Menu";
 import styles from "./InitialPage.module.scss"
-import Banner from "../../components/Banner";
-import Footer from "../../components/Footer";
-import Gallery from "../../components/Gallery";
-import Popular from "../../components/Popular";
+import Banner from "components/Banner";
+import Footer from "components/Footer";
+import Gallery from "components/Gallery";
+import Popular from "components/Popular";
 
 export default function InitialPage() {
     return (
